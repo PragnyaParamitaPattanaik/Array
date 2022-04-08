496608b175c03d36476b6e5e794ef7e05d4f0b05
pragn <pragnya.pattanaik@gmail.com>
2022-04-08 00:10:53 -0700

    commit

---
+++ ArrayExample.java
@@ -0,0 +1,17 @@
+package ArrayPractice;
+
+import java.util.ArrayList;
+
+public class ArrayExample {
+    public static void main (String[] args){
+        //Create ArrayList
+        ArrayList<String> language= new ArrayList<>();
+
+        //Adding Elements to array list
+        language.add("java");
+        language.add("Python");
+        language.add("swift");
+        System.out.println("ArrayList:"+language);
+    }
+
+}
